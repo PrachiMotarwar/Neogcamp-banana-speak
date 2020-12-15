9ca94e3e300ca68ae81f4751feaa565bdd15ea14
Prachi Motarwar <Motarwarprachi@gmail.com>
2020-12-15 18:22:40 +0530

    adding click

---
+++ app.js
@@ -0,0 +1,17 @@
+//console.log("Script is working!! from external")
+
+//input
+//var username = prompt("Give me username")
+
+//processing
+//var msg = ("Hi " + username+ " Welcome to vanilla-js class")
+//output
+
+//alert(msg);'''
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked")
+
+})
